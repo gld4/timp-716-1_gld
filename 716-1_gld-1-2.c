cf60ec59085521e3841d5c56d9e467be38f2a29f
Глазырина <716-1_gld@timp2018.novalocal>
2018-05-20 09:00:10 +0000

    First

---
+++ 716-1_gld-1-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+#include <math.h> 
+
+int main(){ 
+float x, y, z; 
+scanf("%f%f", &x, &y); 
+z=powf(x, y); 
+printf("%f\n", z); 
+return 0; 
+}
---
+++ 716-1_gld-1-3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+        float x, y;
+        scanf("%f%f", &x, &y);
+        printf("%f\n", powf(x, y));
+        return 0;
+}
+
